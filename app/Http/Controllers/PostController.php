f4c769aab759826cb0d03a73193b6730672441a5
BladimirT <bladimirtetzaguic@gmail.com>
2023-12-01 12:12:30 -0600

    Mostrando una publicacion

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -58,7 +58,13 @@ class PostController extends Controller
             'user_id' => auth()->user()->id
         ]);
 
-
         return redirect()->route('posts.index', auth()->user()->username);
+    }
+
+    public function show(User $user, Post $post)
+    {
+        return view('posts.show', [
+            'post' => $post,
+        ]);
     }
 }
